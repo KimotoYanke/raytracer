e6c85fd88eacae9119593bf0d0077a5d52355187
KimotoYanke <kimotoyanke@gmail.com>
2022-08-12 23:45:08 +0900

    HittableListを追加

--- src/hittable.rs
+++ src/hittable.rs
@@ -3,6 +3,7 @@ use crate::{
     vec3::{Point3, Vec3},
 };
 
+#[derive(Clone, Default)]
 pub struct HitRecord {
     pub p: Point3,
     pub normal: Vec3,
@@ -11,6 +12,15 @@ pub struct HitRecord {
 }
 
 impl HitRecord {
+    fn new(p: Point3, normal: Vec3, t: f64, front_face: bool) -> HitRecord {
+        HitRecord {
+            p,
+            normal,
+            t,
+            front_face,
+        }
+    }
+
     pub fn set_face_normal(self: &mut Self, r: &Ray, outward_normal: &Vec3) {
         let front_face = r.direction().dot(outward_normal) < 0.0;
         self.normal = if front_face {
@@ -23,4 +33,39 @@ impl HitRecord {
 
 pub trait Hittable {
     fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool;
+}
+
+pub struct HittableList<'a> {
+    objects: Vec<Box<dyn Hittable + 'a>>,
+}
+
+impl<'a> HittableList<'a> {
+    pub fn new() -> HittableList<'a> {
+        HittableList { objects: vec![] }
+    }
+    pub fn clear(&mut self) {
+        self.objects.clear();
+    }
+
+    pub fn add<T: Hittable + 'a>(&mut self, t: T) {
+        self.objects.push(Box::<T>::new(t))
+    }
+}
+
+impl<'a> Hittable for HittableList<'a> {
+    fn hit(&self, r: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
+        let mut temp_rec = HitRecord::new(Point3::new(0, 0, 0), Vec3::new(0, 0, 0), 0.0, true);
+        let mut hit_anything = false;
+        let mut closest_so_far = t_max;
+
+        for object in &self.objects {
+            if object.hit(r, t_min, closest_so_far, &mut temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                rec.clone_from(&temp_rec);
+            }
+        }
+
+        return hit_anything;
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,23 @@ pub mod ray;
 pub mod sphere;
 pub mod vec3;
 
+use std::f64::INFINITY;
+
+use hittable::{HitRecord, Hittable};
 use ray::Ray;
 use vec3::Color;
 
-use crate::vec3::{Point3, Vec3};
+use crate::{
+    hittable::HittableList,
+    sphere::Sphere,
+    vec3::{Point3, Vec3},
+};
 
-fn ray_color(r: Ray) -> Color {
+fn ray_color<T: Hittable>(r: Ray, world: &T) -> Color {
+    let mut hit_record: HitRecord = HitRecord::default();
+    if world.hit(&r, 0.0, INFINITY, &mut hit_record) {
+        return (hit_record.normal + Color::new(1, 1, 1)) * 0.5;
+    }
     let unit_direction = r.direction().unit();
     let t = 0.5 * (unit_direction.y() + 1.0);
     return (Color::new(1.0, 1.0, 1.0)) * (1.0 - t) + (Color::new(0.5, 0.7, 1.0) * t);
@@ -30,6 +41,11 @@ fn main() {
         origin.clone() - horizontal / 2 - vertical / 2 - Vec3::new(0, 0, FOCAL_LENGTH);
 
     let mut img = image::RgbImage::new(IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    let mut world = HittableList::new();
+    world.add(Sphere::new(Point3::new(0, 0, -1), 0.5));
+    world.add(Sphere::new(Point3::new(0, -100.5, -1), 100.0));
+
     for (x, y, pixel) in img.enumerate_pixels_mut() {
         let y = IMAGE_HEIGHT - 1 - y;
         let u = x as f64 / (IMAGE_WIDTH - 1) as f64;
@@ -38,7 +54,7 @@ fn main() {
             origin,
             lower_left_corner + horizontal * u + vertical * v - origin,
         );
-        let color: Color = ray_color(r);
+        let color: Color = ray_color(r, &world);
         *pixel = color.to_rgb();
     }
     img.save("result.png").unwrap();
